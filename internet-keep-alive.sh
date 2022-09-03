236db41f34df783b254ac1de3075cbdf680dacd8
Surfer2010 <surfer2010@herfort.eu>
2022-09-03 23:54:23 +0200

    Update internet-keep-alive.sh

--- internet-keep-alive.sh
+++ internet-keep-alive.sh
@@ -1,16 +1,19 @@
+#------------------------------------------------------------------------------------
+#---------------root@OpenWrt:~# cat wg-keep-alive.sh--------------------------
+#------------------------------------------------------------------------------------
+
 #!/bin/ash
 # The main script file. I recommend putting into cron, e.g.:
-# */2 * * * * /root/internet-keep-alive/internet-keep-alive.sh
+# */2 * * * * /root/wg-skript/wg-keep-alive.sh
 
 DIR=$( cd $(dirname $0) ; pwd -P )
-LOG_FILE="$DIR/log.txt"
+LOG_FILE="$DIR/log-wg.txt"
 
 OFFLINE_COUNT=$(cat $LOG_FILE | tail -4 | grep OFFLINE | wc -l)
 OFFLINE_COUNT_TRESHOLD=4
 
-SH_DNS_TESTS="$DIR/dns-test.sh"
-SH_RESTART_INTERFACE="$DIR/restart-interface.sh"
-SH_RESTART_ROUTER="$DIR/restart-router.sh"
+SH_WG_TEST="$DIR/wg-test.sh"
+SH_RESTART_INTERFACE="$DIR/wg-restart-interface.sh"
 
 LINES_MAX=11000
 LINES_MIN=6000
@@ -22,20 +25,20 @@ if [[ "$LINES_COUNT" -ge "$LINES_MAX" ]]; then
 fi
 
 # DNS test, it's result defines the ONLINE/OFFLINE state
-`$SH_DNS_TESTS`
+`$SH_WG_TEST`
 
 if [ $? -eq 1 ]; then
-   echo "Ooops, we're offline!"
+   echo "Ooops, Wireguard is down!"
    echo "$(date) OFFLINE > RESTARTING INTERFACE" >> $LOG_FILE
 
-   if [[ "$OFFLINE_COUNT" -ge "$OFFLINE_COUNT_TRESHOLD" ]]; then
-      echo ">> Restarting router.."
-      $SH_RESTART_ROUTER
-   else
-      echo ">> Restarting interface.."
-      $SH_RESTART_INTERFACE
-   fi
+ #  if [[ "$OFFLINE_COUNT" -ge "$OFFLINE_COUNT_TRESHOLD" ]]; then
+ #     echo ">> Checking wireguard connectivity.." >> $LOG_FILE
+ #     $SH_4G_DNS_TEST
+ #  else
+      echo ">> Restarting wireguard-interface.." >> $LOG_FILE
+      #$SH_RESTART_INTERFACE
+ #  fi
 else
-   echo "We're okay!"
+   echo "Wireguard is okay!"
    echo "$(date) ONLINE" >> $LOG_FILE
 fi
